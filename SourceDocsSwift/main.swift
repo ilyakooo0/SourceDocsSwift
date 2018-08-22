50b0899f94faa7249e1fff5c540e8a6b17af6d62
ilyakooo0 <ilyakooo0@gmail.com>
2018-08-22 20:30:53 +0300

    Initial Commit

---
+++ SourceDocsSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SourceDocsSwift
+//
+//  Created by Ilya Kos on 8/22/18.
+//  Copyright © 2018 Ilya Kos. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
